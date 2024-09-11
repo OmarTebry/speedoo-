24ac1d931519d203b4ea79822ad4b26d6b45e221
OmarTebry <omartebry@gmail.com>
2024-09-11 10:00:40 +0300

    swagger fix attempt 1

--- src/main/java/com/transfer/speedotransfer/config/WebSecurityConfig.java
+++ src/main/java/com/transfer/speedotransfer/config/WebSecurityConfig.java
@@ -17,6 +17,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig {
@@ -28,6 +30,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
                 .exceptionHandling(e -> e.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**",
